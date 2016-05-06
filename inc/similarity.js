8b88ac04b563b0a7c11b975101e662f70c7f28c9
Stanislav Zavalishin <stan-coder@gmail.com>
2016-05-06 17:32:51 +0600

    Realize primary methods to separate potential strings that can match with patterns

--- inc/similarity.js
+++ inc/similarity.js
@@ -5,27 +5,65 @@ class Similarity {
 	constructor(input, patterns) {
 		this.input = input;
 		this.patterns = patterns;
+		this.matchList = {
+			1: {
+				input: 'another',
+				patterns: ['anoother', 'azother']
+			}
+		};
 	}
 
 	init() {
-		this.splitInputOnBlocks();
-		this.patterns.forEach((pattern) => {
-
-			this.splitedInput.forEach((blocks) => {
-
-				var matchProbability = 0;
-				console.log(blocks);
-			});
+		this.splitDataOnBlocks();
+		this.splittedInput.forEach((input, key) => {
+			this.compare(input, key)
 		});
 	}
 
-	splitInputOnBlocks() {
-		this.splitedInput = [];
-		this.input.forEach((string) => {
+	compare(input, keyInput) {
+		/**
+		 * Max. acceptable count of words coincidental on pattern
+		 */
+		var maxCoPattern = Math.floor(input.length / 2.661);
 
-			this.splitedInput.push(
-				string.split(/[^\da-z]/i).filter(el => el.length)
-			);
+		/**
+		 * Max. acceptable count of words that is allowed to absent in a sentence
+		 * or do not math with pattern
+		 */
+		var maxAbsent = Math.floor(maxCoPattern / 2);
+
+		this.splittedPatterns.forEach((pattern) => {
+
+			function* Gen(scope) {
+				yield !scope.exposeBasicLength(maxAbsent, input.length, pattern.length);
+
+				console.log([input, pattern]);
+				yield false;
+			}
+
+			for (let value of Gen(this)) {
+				if (value) {
+					return;
+				}
+			}
+		});
+	}
+
+	exposeBasicLength(maxAbsent, i, p) {
+		return p <= i && p >= (i - maxAbsent);
+	}
+
+	splitDataOnBlocks() {
+		['Input', 'Patterns'].forEach((elem) => {
+			var propName = 'splitted' + elem;
+			this[propName] = [];
+
+			this[elem.toLowerCase()].forEach((string) => {
+				var blocks = string.split(/[^\da-z\-_]/i).filter(el => el.length);
+				if (blocks.length > 0) {
+					this[propName].push(blocks);
+				}
+			});
 		});
 	}
 }
